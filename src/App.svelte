81f82c5afec8cd293d8ede5f45fb6668822a6542
ilhamlutfii <mochlutfi1020@gmail.com>
2022-06-28 22:01:40 +0700

    update file

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+// @ts-ignore
+import Facebook from "./lib/Facebook.svelte";
+
+import GoogleSignInButton from "./lib/GoogleSignInButton.svelte";
+
+
+  
+</script>
+
+<main>
+  <GoogleSignInButton/>
+  <Facebook/>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/lib/GoogleSignInButton.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { GoogleLogin } from 'google-login-svelte'
+ 
+    let user = null
+ 
+    const handleSignin = (event) => {
+       user = event.detail.profileObj
+       console.log(`User ${user.name} signed in successfully`)
+    }
+ 
+    const handleFailure = (event) => {
+       const err = event.detail
+       console.error(`Error while signing in: `, err)
+    }
+ </script>
+ 
+ <GoogleLogin 
+    clientId="116260901621-5d7ojp12do5qkcl4peccsela565oq3ue.apps.googleusercontent.com"
+    on:success={handleSignin}
+    on:failure={handleFailure}
+    let:signIn
+ >
+    <button on:click={signIn}>
+       Sign in with Google
+    </button>
+ </GoogleLogin>
+ 
+ {#if user}
+    <h1>Welcome {user.name}</h1>
+ {/if}
